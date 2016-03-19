8e24a1154bcc572031f36e6ea898fbcdb45dae36
fish2013 <894901002@qq.com>
2016-03-19 20:57:06 +0800

    秒拍URL

---
+++ src/main/java/com/bbcow/news/website/miaopai/MiaopaiSpider.java
@@ -0,0 +1,12 @@
+package com.bbcow.news.website.miaopai;
+
+
+public class MiaopaiSpider {
+	private String url = "http://api.miaopai.com/m/shot_channel.json?f_type=v2&likeStat=0&live=1&os=ios&page=1&per=20&suid=4NUB4nrCdVdQFbHH&unique_id=79d273867a9c357b6f978203addcb0b71234284759&version=6.3.1";
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
